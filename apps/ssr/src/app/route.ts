b59368cb10d5d0c24db0cfe5e6d755c8f084b2c8
Linx Han <linxuanhan@student.unimelb.edu.au>
2025-12-03 19:34:50 +1100

    feat: change route.ts

--- apps/ssr/src/app/route.ts
+++ apps/ssr/src/app/route.ts
@@ -3,15 +3,19 @@ import type { NextRequest } from 'next/server'
 
 import { injectConfigToDocument } from '~/lib/injectable'
 
-export const GET = handle
-export const HEAD = handle
-export const OPTIONS = handle
-export const POST = handle
-export const PUT = handle
-export const PATCH = handle
-export const DELETE = handle
+const renderIndex = async () => {
+  const indexHtml = await import('../index.html').then((m) => m.default)
+  const document = new DOMParser().parseFromString(indexHtml, 'text/html')
+  injectConfigToDocument(document)
+  return new Response(document.documentElement.outerHTML, {
+    headers: {
+      'Content-Type': 'text/html',
+      'X-SSR': '1',
+    },
+  })
+}
 
-async function handle(req: NextRequest) {
+const handler = async (req: NextRequest) => {
   if (process.env.NODE_ENV === 'development') {
     return import('./dev').then((m) => m.handler(req))
   }
@@ -20,23 +24,18 @@ async function handle(req: NextRequest) {
     return new Response(null, { status: 404 })
   }
 
-  const acceptHtml = req.headers.get('accept')?.includes('text/html')
-  if (!acceptHtml) return new Response(null, { status: 404 })
+  const acceptsHtml = req.headers.get('accept')?.includes('text/html')
+  if (!acceptsHtml) {
+    return new Response(null, { status: 404 })
+  }
 
   return renderIndex()
 }
 
-async function renderIndex() {
-  // 原始正确路径（位于 ssr output 内）
-  const indexHtml = await import('../index.html').then((m) => m.default)
-
-  const document = new DOMParser().parseFromString(indexHtml, 'text/html')
-  injectConfigToDocument(document)
-
-  return new Response(document.documentElement.outerHTML, {
-    headers: {
-      'Content-Type': 'text/html',
-      'X-SSR': '1',
-    },
-  })
-}
+export const GET = handler
+export const HEAD = handler
+export const OPTIONS = handler
+export const POST = handler
+export const PUT = handler
+export const PATCH = handler
+export const DELETE = handler
